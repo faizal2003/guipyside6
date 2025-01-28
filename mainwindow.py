721465e7495e3397edf801c107e00fffc445e608
faizalSan <49065101+faizal2003@users.noreply.github.com>
2025-01-29 00:32:44 +0700

    table added

--- mainwindow.py
+++ mainwindow.py
@@ -1,7 +1,9 @@
 # This Python file uses the following encoding: utf-8
 import sys
 import ui_src
-from PySide6.QtWidgets import QApplication, QMainWindow, QStackedWidget
+from PySide6.QtCore import *
+from PySide6.QtGui import *
+from PySide6.QtWidgets import *
 
 # Important:
 # You need to run the following command to generate the ui_form.py file
@@ -10,6 +12,10 @@ from PySide6.QtWidgets import QApplication, QMainWindow, QStackedWidget
 from ui_form import Ui_MainWindow
 from ui_mainmenu import Ui_MainMenu
 from ui_tambahdata import Ui_TambahData
+from ui_password import Ui_Password
+from ui_log import Ui_Log
+
+
 
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
@@ -34,7 +40,11 @@ class SecondWindow(QMainWindow):
         self.ui.setupUi(self)
         self.ui.pushButton_5.pressed.connect(self.back_action)
         self.ui.pushButton_2.pressed.connect(self.tambahdata_action)
+        self.ui.pushButton.pressed.connect(self.logbutton_action)
 
+    def logbutton_action(self):
+        print("button log pressed")
+        widget.setCurrentIndex(widget.currentIndex() + 3 )
     def back_action(self):
         # print("button start pressed")
         widget.setCurrentIndex(widget.currentIndex() - 1)
@@ -57,6 +67,92 @@ class TambahData(QMainWindow):
     def testshutdown(self):
         print("button shut down pressed")
 
+class Password(QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.ui = Ui_Password()
+        self.ui.setupUi(self)
+        self.ui.pushButton.pressed.connect(self.back_action)
+        self.ui.lineEdit.setEchoMode(QLineEdit.Password)
+        self.ui.lineEdit.setPlaceholderText("Enter Password")
+        self.ui.pushButton_2.pressed.connect(self.checkpassword)
+
+    def checkpassword(self):
+        if self.ui.lineEdit.text() == "admin":
+            print("Password is correct")
+            self.ui.lineEdit.clear()
+            widget.setCurrentIndex(widget.currentIndex() + 1)
+        else:
+            print("Password is incorrect")
+    
+    def back_action(self):
+        print("button start pressed")
+        widget.setCurrentIndex(widget.currentIndex() - 1)
+
+    def testshutdown(self):
+        print("button shut down pressed")
+        
+class LogWindow(QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.ui = Ui_Log()
+        self.ui.setupUi(self)
+        
+        table_model = MyTableModel(self, data_list, header)
+        self.ui.tableView.setModel(table_model)
+        font = QFont("Courier New", 20, QFont.Bold)
+        self.ui.tableView.setFont(font)
+        self.ui.tableView.setFixedSize(700, 450)
+        self.ui.tableView.resizeColumnsToContents()
+        # self.ui.tableView.setSortingEnabled(True)
+        
+class MyTableModel(QAbstractTableModel):
+    def __init__(self, parent, mylist, header, *args):
+        QAbstractTableModel.__init__(self, parent, *args)
+        self.mylist = mylist
+        self.header = header
+
+    def rowCount(self, parent):
+        return len(self.mylist)
+
+    def columnCount(self, parent):
+        return len(self.mylist[0])
+
+    def data(self, index, role):
+        if not index.isValid():
+            return None
+        elif role != Qt.DisplayRole:
+            return None
+        return self.mylist[index.row()][index.column()]
+
+    def headerData(self, col, orientation, role):
+        if orientation == Qt.Horizontal and role == Qt.DisplayRole:
+            return self.header[col]
+        return None
+
+    def sort(self, col, order):
+        """sort table by given column number col"""
+        self.emit(SIGNAL("layoutAboutToBeChanged()"))
+        self.mylist = sorted(self.mylist,
+            key=operator.itemgetter(col))
+        if order == Qt.DescendingOrder:
+            self.mylist.reverse()
+        self.emit(SIGNAL("layoutChanged()"))
+
+# the solvent data ...
+header = ['No','Nama', ' Tanggal/Waktu', ' Aktivitas']
+# use numbers for numeric data to sort properly
+data_list = [
+(1, "yanto", "12 januari 25/ 11:11:11", "login"),
+(2, "agus sedunia", "25 januari 25/12:12:12", "login"),
+(2, "agus sedunia", "25 januari 25/12:12:12", "login"),
+(2, "agus sedunia", "25 januari 25/12:12:12", "login"),
+(2, "agus sedunia", "25 januari 25/12:12:12", "login"),
+(2, "agus sedunia", "25 januari 25/12:12:12", "login"),
+(2, "agus sedunia", "25 januari 25/12:12:12", "login"),
+(2, "agus sedunia", "25 januari 25/12:12:12", "login"),
+]
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
@@ -64,9 +160,13 @@ if __name__ == "__main__":
     mainwindow=MainWindow()
     mainmenu=SecondWindow()
     tambahdata=TambahData()
+    password=Password()
+    log=LogWindow()
     widget.addWidget(mainwindow)
     widget.addWidget(mainmenu)
+    widget.addWidget(password)
     widget.addWidget(tambahdata)
+    widget.addWidget(log)
     # widget = MainWindow()
     widget.show()
     sys.exit(app.exec())
---
+++ ui_log.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'log.ui'
+##
+## Created by: Qt User Interface Compiler version 6.8.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QHeaderView, QMainWindow, QMenuBar,
+    QPushButton, QSizePolicy, QStatusBar, QTableView,
+    QWidget)
+
+class Ui_Log(object):
+    def setupUi(self, Log):
+        if not Log.objectName():
+            Log.setObjectName(u"Log")
+        Log.resize(1024, 600)
+        Log.setMinimumSize(QSize(1024, 600))
+        Log.setMaximumSize(QSize(1024, 600))
+        Log.setStyleSheet(u"background-image: url(:/menu/START(2) 1.png);\n"
+"background-repeat: false;")
+        self.centralwidget = QWidget(Log)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.pushButton = QPushButton(self.centralwidget)
+        self.pushButton.setObjectName(u"pushButton")
+        self.pushButton.setGeometry(QRect(30, 10, 81, 81))
+        self.pushButton.setStyleSheet(u"background-image: url(:/menu/arrow 1.png);\n"
+"background-repeat:false;\n"
+"border-radius: 10;")
+        self.widget = QWidget(self.centralwidget)
+        self.widget.setObjectName(u"widget")
+        self.widget.setGeometry(QRect(330, 10, 361, 91))
+        self.widget.setStyleSheet(u"background-image: url(:/password/Rectangle 2 log.png);\n"
+"background-repeat: false;")
+        self.tableView = QTableView(self.centralwidget)
+        self.tableView.setObjectName(u"tableView")
+        self.tableView.setGeometry(QRect(150, 190, 561, 231))
+        self.tableView.setStyleSheet(u"background-color: rgb(255,255,255);\n"
+"gridline-color:  rgb(255, 255, 255);\n"
+"color: rgb(0,0,0);")
+        Log.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(Log)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 1024, 21))
+        Log.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(Log)
+        self.statusbar.setObjectName(u"statusbar")
+        Log.setStatusBar(self.statusbar)
+
+        self.retranslateUi(Log)
+
+        QMetaObject.connectSlotsByName(Log)
+    # setupUi
+
+    def retranslateUi(self, Log):
+        Log.setWindowTitle(QCoreApplication.translate("Log", u"MainWindow", None))
+        self.pushButton.setText("")
+    # retranslateUi
+
